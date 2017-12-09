bd235622e31afa622593d0a771f9053cf5d59072
Nick Schwaderer <nick.schwaderer@oceanshq.com>
2017-12-09 23:48:21 +0000

    day two advent of code part two

--- 2_checksum.rb
+++ 2_checksum.rb
@@ -27,10 +27,45 @@ class ChristmasChecksum
   end
 end
 
-# Benchmark.bm do |x|
-#   x.report('ChristmasChecksum:') { ChristmasChecksum.new(input) }
-# end
+class ChristmasEvenlyDivisible
+  attr_accessor :divisible_total, :input_array
+
+  def initialize(input)
+    @input_array = input
+    @divisible_total = 0
+    inspect_rows
+    puts "Total evenly divisible is #{divisible_total}"
+  end
+
+  private
+
+  def evenly_divisible_check(number_one, number_two)
+    @divisible_total += (number_one.to_f / number_two.to_f) if (number_one.to_f / number_two.to_f) % 1 == 0
+  end
+
+  def inspect_rows
+    @input_array.each do |row|
+      row.each do |element|
+        row.each do |second_element|
+          next if element == second_element
+          evenly_divisible_check(element, second_element)
+        end
+      end
+    end
+  end
+end
+
+Benchmark.bm do |x|
+  x.report('ChristmasChecksum:') { ChristmasChecksum.new(input) }
+end
+
+Benchmark.bm do |x|
+  x.report('ChristmasEvenlyDivisible:') { ChristmasEvenlyDivisible.new(input) }
+end
 
 # ChristmasChecksum:Total checksum is xxxxx
-#        user     system      total        real
-  # 0.000000   0.000000   0.000000 (  0.000129)
+#       user     system      total        real
+# 0.000000   0.000000   0.000000 (  0.000129)
+# ChristmasEvenlyDivisible:Total evenly divisible is xxxxx
+#      user     system      total        real
+# 0.000000   0.000000   0.000000 (  0.001000)
